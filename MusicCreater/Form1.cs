a35f9ca7672d524486e36884f0ba35698f86fbc1
ben2133 <ben.tn2002@mail.ru>
2023-06-15 12:26:21 +0300

    Перевод основных функции работы с аудио в отдельный класс

--- MusicCreater/Form1.cs
+++ MusicCreater/Form1.cs
@@ -1,111 +1,90 @@
 ﻿using System;
-using System.IO;
-using NAudio.Wave;
 using System.Windows.Forms;
 
 namespace MusicCreater
 {
     public partial class Form1 : Form
     {
-        // Class Fild to Play:
-        private AudioFileReader audioFile;
-        private WaveOutEvent outputDevice;
-
-        // Class Fild to Record:
-        private WaveFileWriter RecordedAudioWriter;
-        private WasapiLoopbackCapture CaptureInstance;
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        // Nota Player:
-        public void musicPlayer(string music_name)
-        {
-            if (File.Exists(music_name)) {
-                this.outputDevice = new WaveOutEvent();
-                this.audioFile = new AudioFileReader(music_name);
-                this.outputDevice.Init(this.audioFile);
-                this.outputDevice.Volume = ((float)this.audio_volume.Value / 100);
-                this.outputDevice.Play();
-            } else
-            {
-                MessageBox.Show("Файлы звучание клавиш не найден");
-            }
+        
+        // My Nota_Play_Class
+        public Nota_Play_Class Nota_PLay = new Nota_Play_Class();
 
-        }
 
         // Nota DO:
         private void button_do_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/nota-do.mp3");
+            this.Nota_PLay.do_Play((float)this.audio_volume.Value);
         }
 
         // Nota RE:
         private void button_re_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/re.mp3");
+            this.Nota_PLay.re_Play((float)this.audio_volume.Value);
         }
 
         // Nota MI:
         private void button_mi_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/mi.mp3");
+            this.Nota_PLay.mi_Play((float)this.audio_volume.Value);
         }
 
         // Nota FA:
         private void button_fa_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/nota-fa.mp3");
+            this.Nota_PLay.fa_Play((float)this.audio_volume.Value);
         }
 
         // Nota SOL:
         private void button_sol_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/sol.mp3");
+            this.Nota_PLay.sol_Play((float)this.audio_volume.Value);
         }
 
         // Nota LYA:
         private void button_lya_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/lja.mp3");
+            this.Nota_PLay.lya_Play((float)this.audio_volume.Value);
         }
 
         // Nota SI:
         private void button_si_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/si.mp3");
+            this.Nota_PLay.si_Play((float)this.audio_volume.Value);
         }
         
         // Note C# Db:
         private void button_Dob_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/c#.mp3");
+            this.Nota_PLay.c_db_Play((float)this.audio_volume.Value);
         }
 
         // Note D# Eb:
         private void button_Eb_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/d#.mp3");
+            this.Nota_PLay.d_eb_Play((float)this.audio_volume.Value);
         }
 
         // Note F# Gb:
         private void button_Gb_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/f#.mp3");
+            this.Nota_PLay.f_gb_Play((float)this.audio_volume.Value);
         }
 
         // Note G# Ab:
         private void button_Ab_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/g#.mp3");
+            this.Nota_PLay.g_ab_Play((float)this.audio_volume.Value);
         }
 
         // Note A# Bb:
         private void button_Bb_Click(object sender, EventArgs e)
         {
-            this.musicPlayer(@"lib_music/a#.mp3");
+            this.Nota_PLay.a_bb_Play((float)this.audio_volume.Value);
         }
 
         // Button Play recorded audio:
@@ -114,12 +93,12 @@ namespace MusicCreater
             if (this.play_btn.Text == "Воспроизвести")
             {
                 this.play_btn.Text = "Останвить";
-                this.musicPlayer((string)AppDomain.CurrentDomain.BaseDirectory + "/RecordAudio/system_recorded_audio.wav");
+                this.Nota_PLay.Play_M(true, this.audio_volume.Value);
             }
             else
             {
                 this.play_btn.Text = "Воспроизвести";
-                this.outputDevice.Stop();
+                this.Nota_PLay.Play_M(false, this.audio_volume.Value);
             }
         }
 
@@ -129,44 +108,12 @@ namespace MusicCreater
             if (this.record_btn.Text == "Записать") // Start Record:
             {
                 this.record_btn.Text = "Остановить";
-
-                // Define the output wav file of the recorded audio
-                string outputFilePath = "/RecordAudio/system_recorded_audio.wav";
-
-                if (!Directory.Exists("/RecordAudio"))
-                {
-                    Directory.CreateDirectory("/RecordAudio");
-                }
-
-                // Redefine the capturer instance with a new instance of the LoopbackCapture class
-                this.CaptureInstance = new WasapiLoopbackCapture();
-
-                // Redefine the audio writer instance with the given configuration
-                this.RecordedAudioWriter = new WaveFileWriter(outputFilePath, CaptureInstance.WaveFormat);
-
-                // When the capturer receives audio, start writing the buffer into the mentioned file
-                this.CaptureInstance.DataAvailable += (s, a) =>
-                {
-                    this.RecordedAudioWriter.Write(a.Buffer, 0, a.BytesRecorded);
-                };
-
-                // When the Capturer Stops
-                this.CaptureInstance.RecordingStopped += (s, a) =>
-                {
-                    this.RecordedAudioWriter.Dispose();
-                    this.RecordedAudioWriter = null;
-                    this.CaptureInstance.Dispose();
-                };
-
-                // Start recording:
-                this.CaptureInstance.StartRecording();
+                this.Nota_PLay.rec_Audio(true);                
             }
             else // Stop Record:
             {
                 this.record_btn.Text = "Записать";
-
-                // Stop Record:
-                this.CaptureInstance.StopRecording();
+                this.Nota_PLay.rec_Audio(false);               
             }
         }
 
@@ -177,62 +124,62 @@ namespace MusicCreater
             {
                 // Nota Do:
                 case Keys.A:
-                    this.musicPlayer(@"lib_music/nota-do.mp3");
+                    this.Nota_PLay.do_Play((float)this.audio_volume.Value);
                     break;
 
                 // Nota Re:
                 case Keys.S:
-                    this.musicPlayer(@"lib_music/re.mp3");
+                    this.Nota_PLay.re_Play((float)this.audio_volume.Value);
                     break;
 
                 // Nota Mi:
                 case Keys.D:
-                    this.musicPlayer(@"lib_music/mi.mp3");
+                    this.Nota_PLay.mi_Play((float)this.audio_volume.Value);
                     break;
 
                 // Nota Fa:
                 case Keys.F:
-                    this.musicPlayer(@"lib_music/nota-fa.mp3");
+                    this.Nota_PLay.fa_Play((float)this.audio_volume.Value);
                     break;
 
                 // Nota Sol:
                 case Keys.G:
-                    this.musicPlayer(@"lib_music/sol.mp3");
+                    this.Nota_PLay.sol_Play((float)this.audio_volume.Value);
                     break;
 
                 // Nota Lya:
                 case Keys.H:
-                    this.musicPlayer(@"lib_music/lja.mp3");
+                    this.Nota_PLay.lya_Play((float)this.audio_volume.Value);
                     break;
 
                 // Nota Si:
                 case Keys.J:
-                    this.musicPlayer(@"lib_music/si.mp3");
+                    this.Nota_PLay.si_Play((float)this.audio_volume.Value);
                     break;
 
                 // Nota C#:
                 case Keys.W:
-                    this.musicPlayer(@"lib_music/c#.mp3");
+                    this.Nota_PLay.c_db_Play((float)this.audio_volume.Value);
                     break;
 
                 // Nota D#:
                 case Keys.R:
-                    this.musicPlayer(@"lib_music/d#.mp3");
+                    this.Nota_PLay.d_eb_Play((float)this.audio_volume.Value);
                     break;
 
                 // Nota F#:
                 case Keys.U:
-                    this.musicPlayer(@"lib_music/f#.mp3");
+                    this.Nota_PLay.f_gb_Play((float)this.audio_volume.Value);
                     break;
 
                 // Nota G#:
                 case Keys.I:
-                    this.musicPlayer(@"lib_music/g#.mp3");
+                    this.Nota_PLay.g_ab_Play((float)this.audio_volume.Value);
                     break;
 
                 // Nota A#:
                 case Keys.O:
-                    this.musicPlayer(@"lib_music/a#.mp3");
+                    this.Nota_PLay.a_bb_Play((float)this.audio_volume.Value);
                     break;
             }
         }
---
+++ MusicCreater/Nota_Play_Class.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using NAudio.Wave;
+using System.Windows.Forms;
+
+namespace MusicCreater
+{
+    public class Nota_Play_Class
+    {
+        // Class Fild to Play:
+        private AudioFileReader audioFile;
+        private WaveOutEvent outputDevice;
+
+        // Class Fild to Record:
+        private WaveFileWriter RecordedAudioWriter;
+        private WasapiLoopbackCapture CaptureInstance;
+
+        // Nota Player:
+        private void musicPlayer(string music_name, float audio_volume)
+        {
+            if (File.Exists(music_name))
+            {
+                this.outputDevice = new WaveOutEvent();
+                this.audioFile = new AudioFileReader(music_name);
+                this.outputDevice.Init(this.audioFile);
+                this.outputDevice.Volume = (audio_volume / 100);
+                this.outputDevice.Play();
+            }
+            else
+            {
+                MessageBox.Show("Файлы звучание клавиш не найден");
+            }
+        }
+
+        // Nota DO:
+        public void do_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/nota-do.mp3", ad_volume);
+        }
+
+        // Nota RE:
+        public void re_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/re.mp3", ad_volume);
+        }
+
+        // Nota MI:
+        public void mi_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/mi.mp3", ad_volume);
+        }
+
+        // Nota FA:
+        public void fa_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/nota-fa.mp3", ad_volume);
+        }
+
+        // Nota SOL:
+        public void sol_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/sol.mp3", ad_volume);
+        }
+
+        // Nota LYA:
+        public void lya_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/lja.mp3", ad_volume);
+        }
+
+        // Nota SI:
+        public void si_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/si.mp3", ad_volume);
+        }
+
+        // Nota C# Db:
+        public void c_db_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/c#.mp3", ad_volume);
+        }
+
+        // Nota D# Eb:
+        public void d_eb_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/d#.mp3", ad_volume);
+        }
+
+        // Nota F# Gb:
+        public void f_gb_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/f#.mp3", ad_volume);
+        }
+
+        // Nota G# Ab:
+        public void g_ab_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/g#.mp3", ad_volume);
+        }
+
+        // Nota A# Bb:
+        public void a_bb_Play(float ad_volume)
+        {
+            this.musicPlayer(@"lib_music/mi.mp3", ad_volume);
+        }
+
+        // Play Recording Audio
+        public void Play_M(bool Start, float ad_volume)
+        {
+            if (Start == true)
+            {
+                this.musicPlayer((string)AppDomain.CurrentDomain.BaseDirectory + "/RecordAudio/system_recorded_audio.wav", ad_volume);
+            } else
+            {
+                this.outputDevice.Stop();
+            }
+        }
+
+        // Recordin Audio
+        public void rec_Audio(bool Start)
+        {
+            if (Start == true)
+            {
+                // Define the output wav file of the recorded audio
+                string outputFilePath = "/RecordAudio/system_recorded_audio.wav";
+
+                if (!Directory.Exists("/RecordAudio"))
+                {
+                    Directory.CreateDirectory("/RecordAudio");
+                }
+
+                // Redefine the capturer instance with a new instance of the LoopbackCapture class
+                this.CaptureInstance = new WasapiLoopbackCapture();
+
+                // Redefine the audio writer instance with the given configuration
+                this.RecordedAudioWriter = new WaveFileWriter(outputFilePath, CaptureInstance.WaveFormat);
+
+                // When the capturer receives audio, start writing the buffer into the mentioned file
+                this.CaptureInstance.DataAvailable += (s, a) =>
+                {
+                    this.RecordedAudioWriter.Write(a.Buffer, 0, a.BytesRecorded);
+                };
+
+                // When the Capturer Stops
+                this.CaptureInstance.RecordingStopped += (s, a) =>
+                {
+                    this.RecordedAudioWriter.Dispose();
+                    this.RecordedAudioWriter = null;
+                    this.CaptureInstance.Dispose();
+                };
+
+                // Start recording:
+                this.CaptureInstance.StartRecording();
+            } else
+            {
+                // Stop Record:
+                this.CaptureInstance.StopRecording();
+            }
+        }
+    }
+}
